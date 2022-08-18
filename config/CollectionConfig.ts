1acab19dcb84f3b13b87989f1ede07aa5db96436
Wang Wenlin <sopl.wang@gmail.com>
2022-08-18 19:55:17 +0800

    chore: fix format

--- config/CollectionConfig.ts
+++ config/CollectionConfig.ts
@@ -13,10 +13,10 @@ const CollectionConfig: CollectionConfigInterface = {
   // The contract name can be updated using the following command:
   // yarn rename-contract NEW_CONTRACT_NAME
   // Please DO NOT change it manually!
-  contractName: 'YourNftToken',
-  tokenName: 'My NFT Token',
-  tokenSymbol: 'MNT',
-  hiddenMetadataUri: 'ipfs://__CID__/hidden.json',
+  contractName: "YourNftToken",
+  tokenName: "My NFT Token",
+  tokenSymbol: "MNT",
+  hiddenMetadataUri: "ipfs://__CID__/hidden.json",
   maxSupply: 7777,
   royaltyFraction: 250,
   whitelistSale: {
